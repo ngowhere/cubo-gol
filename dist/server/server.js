0443bec1f24aa6deddcbe89ff0e602d4cd5e9fc6
Zahir Saiyed <zahir19saiyed@gmail.com>
2023-10-13 21:39:28 -0400

    Changed port

--- dist/server/server.js
+++ dist/server/server.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const http_1 = __importDefault(require("http"));
-const port = 3000;
+const port = process.env.PORT || 3000;
 class App {
     constructor(port) {
         this.port = port;
